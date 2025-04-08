fe1dbd033a681864e74b571198ec777e3fe12c94
好自为之 <zhaoziweizhi@gmail.com>
2025-04-08 18:01:32 +0800

    feat: support version suffix
    example: `archiver dist --version-suffix`

--- cli.ts
+++ cli.ts
@@ -1,3 +1,6 @@
+import fs from 'node:fs'
+import path from 'node:path'
+import process from 'node:process'
 import { cac } from 'cac'
 import { zip } from '.'
 import { version } from './package.json'
@@ -7,9 +10,10 @@ const cli = cac('archiver')
 cli
   .command('[input] <rename>', '压缩指定目录或文件')
   .option('--output', '输出文件 - name of the output zip file')
+  .option('--version-suffix', '版本后缀', { default: false })
   .option('--time-suffix', '时间后缀', { default: false })
   .action(async (input, rename, options) => {
-    const suffix = `-${new Date().toLocaleString('zh', {
+    const timeSuffix = `-${new Date().toLocaleString('zh', {
       year: 'numeric',
       month: '2-digit',
       day: '2-digit',
@@ -21,13 +25,34 @@ cli
 
     const output = [
       rename || options.output || input,
-      options.timeSuffix ? suffix : '',
+      getVersionSuffix(options.versionSuffix),
+      options.timeSuffix ? timeSuffix : '',
       '.zip',
     ].join('')
 
     await zip(input, output)
   })
-
+cli.command('list', '列出压缩包中的文件')
 cli.help()
 cli.version(version)
 cli.parse()
+
+function getVersionSuffix(value: any) {
+  // --version-suffix
+  if (value === true) {
+    const packageJsonPath = path.resolve(process.cwd(), 'package.json')
+    if (fs.existsSync(packageJsonPath)) {
+      const packageJsonText = fs.readFileSync(packageJsonPath, 'utf8')
+      const packageJson = JSON.parse(packageJsonText)
+      if (packageJson.version) {
+        return `-v${packageJson.version}`
+      }
+    }
+  }
+  // --version-suffix [VERSION]
+  if (typeof value === 'string') {
+    return `-v${value}`
+  }
+  // null
+  return ''
+}
